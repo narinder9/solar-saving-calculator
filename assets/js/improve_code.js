ae53e56e2d6a814bd0dbf758a73ee5bd35949903
rohit <rohit.coolplugins@gmail.com>
2023-06-28 11:55:40 +0530

    form fields highlights with red color if not selected

--- assets/js/improve_code.js
+++ assets/js/improve_code.js
@@ -1,5 +1,5 @@
 function hideShowFields(t) {
-    // $(".chk-grp").val("").attr("disabled", true).hide();
+    $(".chk-grp").val("").attr("disabled", true).hide();
     $(".txt_option").hide();
     var panel_based_on=$('#calculation_type').val();
     if (panel_based_on === "total_roof_area" && panel_based_on) {
@@ -11,7 +11,7 @@ function hideShowFields(t) {
         $(".kw_capacity").show().attr("disabled", false);
         $("#total_roof_area, #roof_area_type").prop("checked", false);
     }
-    console.log($('#calculation_type').val());
+ 
 }
 
 function calculateData() {
@@ -62,36 +62,33 @@ function calculateData() {
     $("#output_co2").text(Math.round(co2emisRed25Year));
 }
 
-// function validate() {
-//     var isCheckedOne = $('#total_roof_area').prop('checked');
-//     var isCheckedTwo = $('#panel_capacity').prop('checked');
-    
-//     if (isCheckedOne || isCheckedTwo) {
-//         $('#errorValidation').text('');
-//         calculateData();
-//     } else {
-//         $('#errorValidation').text('Required.');
-//     }
-// }
+const check_roof_select = function(target){
+    const panel_based_on = $('#calculation_type').val();
+    if (panel_based_on === "total_roof_area" || panel_based_on === "panel_capacity" ){
+       return true;
+    } 
+
+    return false;
+}
 
 $(document).ready(function() {
 
     jQuery("#frm").validate({
-        // errorElement: 'span',
-        // errorClass: 'error_msg',
-        // errorContainer: "error_msg",
-        // errorPlacement: function(){
-        //     return false;  // suppresses error message text
-        // },
+     
         rules: {
             roof_area_txt:{
-                required:'#total_roof_area:checked',
-                number: true
+                required:true,
+                number: true,
+                depends:check_roof_select(),
+               
+                
             } ,
             capacity_txt: 
             {
-                required:'#panel_capacity:checked',
-                number: true
+                // required:'#panel_capacity:checked',
+                required:true,
+                number: true,
+                depends:check_roof_select(),
             },
             electricity_txt: {
                required: true,
@@ -103,103 +100,10 @@ $(document).ready(function() {
             capacity_txt: 'Please Enter Numeric Value',
             electricity_txt: 'Please Enter Numeric Value',
          },
-         highlight: function(element) {
-            $(element).parent().addClass('has-error');
-          },
-          unhighlight: function(element) {
-            $(element).parent().removeClass('has-error');
-          },
-        //   errorElement: 'span',
-        //   errorClass: 'validation-error-message help-block form-helper bold',
-        //   errorPlacement: function(error, element) {
-        //     if (element.parent('.input-group').length) {
-        //       error.insertAfter(element.parent());
-        //     } else {
-        //       error.insertAfter(element);
-        //     }
-        //   },
+     
          submitHandler: function(form) {
             calculateData();
          }
      });
-     
-    // $('#roof_area_txt').keyup(function () { 
-    //     this.value = this.value.replace(/[^0-9\.]/g,'');
-    // });
-  
-    // $('#capacity_txt').keyup(function () { 
-    //     this.value = this.value.replace(/[^0-9\.]/g,'');
-    // });
-    // $('#electricity_txt').keyup(function () { 
-    //     this.value = this.value.replace(/[^0-9\.]/g,'');
-    // });
-
-    // roof_area_error
-    // kw_capacity_error
-    // electricity_unit_error
-
-    // $('.numericval').on('input', function (event) { 
-    //     // this.value = this.value.replace(/[^0-9\.]/g, '');
-    //     if(!this.value.match(/[^0-9\.]/g))
-    //     {
-
-        
-    //     if(event.id=='#roof_area_txt')
-    //     {
-    //         // $("#roof_area_error").style="display:block";
-    //         $("#roof_area_error").attr("style", "display:block")
-    //     }
-         
-    //      if(event.id=='#roof_area_txt')
-    //      {
-    //         $("#kw_capacity_error").show();
-    //      }
-    //      if(event.id=='#electricity_txt')
-    //      {
-    //         $("#electricity_unit_error").show();
-    //      }
-    //     }
-    // });
-    // $('#frm').on('submit', function(e) {
-    //     e.preventDefault();
-    //     // $('#calculator_modal').modal('show');
-    //     var roof_area_value = $('#roof_area_txt').val();
-    //     var size_capacity = $('#capacity_txt').val();
-    //     var price_unit = $('#electricity_txt').val();
-    //     console.log(roof_area_value);
-    //     console.log(size_capacity);
-    //     console.log(price_unit);
-    //     var numeric_reg=(/[^0-9\.]/g);
-    //     var flag=true;
-    //     if(roof_area_value && numeric_reg.test(roof_area_value)=== false)
-    //     {
-    //         // $(".roof_area_error").show();
-    //     console.log(1);
-    //         // $(".roof_area_error").attr("style", "display:block");
-    //         // $(".roof_area_error").css("display", "block");
-    //        document.getElementsByClassName("roof_area_error")[0].style.display = "none";
-    
-    //         flag= false;
-    //     }
-        // else if(size_capacity && numeric_reg.test(size_capacity)=== false)
-        // {
-        //     console.log(2);
-        //     // $(".kw_capacity_error").show();
-        //     // $(".kw_capacity_error").attr("style", "display:block");
-        //     document.getElementsByClassName("kw_capacity_error")[0].style.display = "none";
-        //     flag= false;
-        // }
-    //     else if(numeric_reg.test(price_unit)=== false)
-    //     {
-    //         console.log(3);
-    //         // $(".electricity_unit_error").show();
-    //         // $(".electricity_unit_error").attr("style", "display:block");
-    //         document.getElementsByClassName("electricity_unit_error")[0].style.display = "none";
-    //         flag= false;
-    //     }
-      
-    //     return  flag;
-    // });
-
     
 });
